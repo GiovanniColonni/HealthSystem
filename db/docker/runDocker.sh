9997ea0c8de65318b6483fd4a137c13560f2df62
Luca Valentini <luca97vale@gmail.com>
2021-04-02 11:21:53 +0200

    added db connection in mainServer to remote raspberry.

--- db/docker/runDocker.sh
+++ db/docker/runDocker.sh
@@ -1,1 +1,1 @@
-docker run --name mysql-monitoring -p 3306:3306 -v /home/luca/Downloads/test-mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=pwd -d mysql
+docker run --name mysql-monitoring -p 3306:3306 -v /home/pi/embedded/docker:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=pwd -d mysql
