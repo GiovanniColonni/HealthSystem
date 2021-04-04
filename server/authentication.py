24f53b0704348d525de09a660d614976eabdca6d
GiovanniColonni <giovacolo97@gmail.com>
2021-04-04 16:01:06 +0200

    removed old code

--- server/authentication.py
+++ server/authentication.py
@@ -20,34 +20,22 @@ client = WebApplicationClient(GOOGLE_CLIENT_ID)
 with app.app_context():
 	login_manager.init_app(app)
 
-def get_google_provider_cfg():
-	return requests.get(GOOGLE_DISCOVERY_URL).json()
 
-@authentication.route('/login1',methods=['GET','POST'])
-def login1():
-	if(request.method=='POST'):
-		google_provider_cfg = get_google_provider_cfg()
-		authorization_endpoint = google_provider_cfg["authorization_endpoint"] # point to hit for authenticate
-
-		request_uri = client.prepare_request_uri(
-		   authorization_endpoint,
-		   redirect_uri = "http://127.0.0.1:5000",
-		   scope = ["openid","email","profile"]
-		)
-			
-		return redirect(request_uri) 
-		#print(request_uri)
-		#return request_uri 
 @authentication.route('/login',methods=['POST','GET'])
 def login():
-	print(request)
+	
 	id_token = request.form.get('id_token')
+	email = request.form.get('email')
+	googleId = request.form.get('googleId')
 
 	if id_token is None:
 		print("NO ID token provided")
 		return "NO ID token provided",201 # cambiare in httpstatus.Forbidden
 
 	# convert token into identity
+
+
+
 	return "token ok",200
 
 
