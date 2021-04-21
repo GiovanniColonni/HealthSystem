dd13decdfd2dfcde9be55d0f1680b93c9524a44a
GiovanniColonni <giovacolo97@gmail.com>
2021-04-21 11:09:06 +0200

    prova su raspberry

--- totem/app.py
+++ totem/app.py
@@ -14,16 +14,19 @@ app.config['MQTT_BROKER_URL'] = MQTT_BROKER_URL
 app.config['MQTT_BROKER_PORT'] =  MQTT_BROKER_PORT
 app.config['MQTT_REFRESH_TIME'] = MQTT_REFRESH_TIME
 
+
 api = Api(app=app,title="Totem")
 
 with app.app_context() :
     from blueprint.auth import auth
+    from blueprint.publisher import publisher
 
 app.register_blueprint(auth)
-
+app.register_blueprint(publisher)
 
 
 
 if __name__ == "__main__":
-
-    app.run(debug=False)
+    # use_reloader is usefull only in debug mode, and it's a way to reload the code when
+    # it changes (like nodemon) but with flask MQTT must be disabled otherwise will fail
+    app.run(debug=False,use_reloader=False)
--- totem/blueprint/publisher.py
+++ totem/blueprint/publisher.py
@@ -1,9 +1,13 @@
-from flask import blueprints, current_app as app
+from flask import Blueprint, current_app as app
 from flask_restx import Resource,Api
+from flask_mqtt import Mqtt, MQTT_ERR_SUCCESS
 
-publisher = blueprints("publisher",__name__)
+publisher = Blueprint("publisher",__name__)
 publisher_api = Api(publisher)
 
+mqtt = Mqtt(app)
+
+# mi identifico al server MQTT oppure uso il server come proxy ?
 @publisher_api.route("/Measure")
 class Publisher(Resource):
     
@@ -12,4 +16,11 @@ class Publisher(Resource):
         # l'idea è di creare un thead che ascolti la seriale e, dopo lo start dato
         # dalla seriale, ad ogni misura letta,
         # la invia al broker fino a che dalla seriale non riceve lo stop
-        pass
+        result, mid  = mqtt.publish("prova/provaTopic","messaggio",retain=True)
+        
+        # mid = messagge ID 
+        # result = the error / success code 
+        if(result == MQTT_ERR_SUCCESS):
+            return "ok",200
+        else:
+            return "not ok",500
