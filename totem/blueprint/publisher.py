3917255e90fcd55cd1638412e0bd389777b82592
GiovanniColonni <giovacolo97@gmail.com>
2021-04-21 12:20:52 +0200

    aggiunto thread per fare la misura

--- totem/blueprint/publisher.py
+++ totem/blueprint/publisher.py
@@ -2,25 +2,46 @@ from flask import Blueprint, current_app as app
 from flask_restx import Resource,Api
 from flask_mqtt import Mqtt, MQTT_ERR_SUCCESS
 
+import threading
+import time
+
+from http import HTTPStatus
+
 publisher = Blueprint("publisher",__name__)
 publisher_api = Api(publisher)
 
 mqtt = Mqtt(app)
+
 
 # mi identifico al server MQTT oppure uso il server come proxy ?
 @publisher_api.route("/Measure")
 class Publisher(Resource):
     
     def get(self):
-        # una get qui significa che è stata richiesta una misura
-        # l'idea è di creare un thead che ascolti la seriale e, dopo lo start dato
-        # dalla seriale, ad ogni misura letta,
-        # la invia al broker fino a che dalla seriale non riceve lo stop
-        result, mid  = mqtt.publish("prova/provaTopic","messaggio",retain=True)
         
-        # mid = messagge ID 
-        # result = the error / success code 
-        if(result == MQTT_ERR_SUCCESS):
-            return "ok",200
+        # deamon = True so rthe program isn't prevent to exit
+        '''
+        if(MeasurementInProgress == False):
+            t = threading.Thread(target=self.takeMeasure,daemon=True)
+            t.start()
+            return "starting measurement",HTTPStatus.OK
+            MeasurementInProgress = True
         else:
-            return "not ok",500
+            return "measurerement in progress ",HTTPStatus.CONTINUE    
+        '''
+        t = threading.Thread(target=self.takeMeasure,daemon=True)
+        t.start()
+        return "starting measurement",HTTPStatus.OK
+           
+    def takeMeasure(self):
+        c = 0
+        
+        mqtt.publish("prova/provaThread","Start message trasmission",retain=True)
+        
+        for c in range(256):
+        
+            time.sleep(1)
+            result,mid = mqtt.publish("prova/provaThread",f"messaggio n {c}",retain=True)
+        
+        mqtt.publish("prova/provaThread","End message trasmission",retain=True)
+        
