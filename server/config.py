6e3e86e1fbeb1176842795e393c4496d691f8d3e
luca97vale <51403422+luca97vale@users.noreply.github.com>
2021-04-22 11:33:50 +0200

    Update config.py

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.urandom(24)
 GOOGLE_CLIENT_ID = "844949237967-h0pnqs3orkq4159ngua6s4jp0fdqatl4.apps.googleusercontent.com"
 
 # DB constans
-DB_HOST_ADDRESS = '82.50.172.4'
+DB_HOST_ADDRESS = '79.50.224.210'
 DB_USER = 'root'
 DB_PASSWORD = 'pwd'
 DB_NAME = 'remoteMonitoring'
