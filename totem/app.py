d517aef13704e271957c1c3a06b92ea37af7e1a1
GiovanniColonni <giovacolo97@gmail.com>
2021-04-23 16:32:56 +0200

    tolto support a flask mqtt

--- totem/app.py
+++ totem/app.py
@@ -1,38 +1,21 @@
 from flask import Flask
 from flask_restx import Api
-from flask_mqtt import Mqtt
 
-from config import SECRET_KEY,TOPIC_NAME,MQTT_BROKER_PORT,MQTT_BROKER_URL,MQTT_REFRESH_TIME,FLASK_HOST
+from config import SECRET_KEY,FLASK_HOST
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = SECRET_KEY
 app.config['REMEMBER_COOKIE_HTTPONLY'] = True
 
-
-# MQTT configuration
-
-MQTT_CLIENT_ID = "Client-flask" # questo serve soltanto per prova, dovrà essere il google id
-MQTT_USERNAME = "username" # solo prova, da sostituire con username di google
-MQTT_PASSWORD = "password" # da sostituire con una generata random dal server e memorizzata
-
-
-app.config['MQTT_BROKER_URL'] = MQTT_BROKER_URL 
-app.config['MQTT_BROKER_PORT'] =  MQTT_BROKER_PORT
-app.config['MQTT_REFRESH_TIME'] = MQTT_REFRESH_TIME
-
-app.config['MQTT_CLIENT_ID'] = MQTT_CLIENT_ID 
-app.config['MQTT_USERNAME'] = MQTT_USERNAME 
-app.config['MQTT_PASSWORD'] = MQTT_PASSWORD 
-
 api = Api(app=app,title="Totem")
 
 with app.app_context() :
     from blueprint.auth import auth
-    from blueprint.publisher import publisher
+    from blueprint.readSensor import readSensor
 
 app.register_blueprint(auth)
-app.register_blueprint(publisher)
+app.register_blueprint(readSensor)
 
 
 
---
+++ totem/blueprint/readSensor.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, current_app as app
+from flask_restx import Resource,Api
+
+import threading
+import time
+import serial
+
+from http import HTTPStatus
+import json
+
+from config import SERIAL_PORT, SERIAL_BOUND_SPEED, MQTT_QOS
+
+readSensor = Blueprint("readSensor",__name__)
+readSensor_api = Api(readSensor)
+
+
+@readSensor_api.route("/Measure")
+class Publisher(Resource):
+    
+    def get(self):
+        
+        # deamon = True so rthe program isn't prevent to exit
+        t = threading.Thread(target=self.takeMeasure,daemon=True)
+        t.start()
+        return "starting measurement",HTTPStatus.OK
+           
+    def takeMeasure(self):
+    
+        ser = serial.Serial(SERIAL_PORT,SERIAL_BOUND_SPEED,timeout=1) # controllare eventuali errori   
+        ser.flush()
+        print("[Flask] Start sensor trasmission")
+        
+        cont = 1
+        while cont:
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').rstrip()
+                data = json.loads(line)
+                
+                if(line == "Stop"):
+                    print(f"[Flask] End sensor trasmission")
+                
+                    return 0
+        
