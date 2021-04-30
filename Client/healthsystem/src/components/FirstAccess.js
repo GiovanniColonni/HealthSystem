7dade56a926c6c6431f4227b5b9fc6e8033c7697
GiovanniColonni <giovacolo97@gmail.com>
2021-04-30 23:51:39 +0200

    creata form e API per first access

--- Client/healthsystem/src/components/FirstAccess.js
+++ Client/healthsystem/src/components/FirstAccess.js
@@ -7,33 +7,41 @@ function FirstAccess({user}){
 
     let [userType,setUserType] = useState("")
     let [name,setName] = useState("")
-    let [Surname,setSurname] = useState("")
-    let [date,setdate] = useState("")
+    let [surname,setSurname] = useState("")
     let [CF,setCF] = useState("")
-
+    let [birthday,setBirthday] = useState("")
 
     let buttonClick = (e) => {
       setUserType(e)
-      /*
-      let googleId = user["googleId"]
-      let type = e
-      API.changeUserType(user["googleId"],type)
-        .then(console.log("ok"))
-        .catch(console.log("no ok"))
-      return
-      */
     }
     
     let submit = () =>{
+      API.submitFirstAccess(user["googleId"],name,surname,birthday,CF,userType)
+        .then()
+        .catch()
+    }
+    let onChangeName = (e) => {
+        setName(e.target.value)
+    }
+    let onChangeSurname = (e) => {
+      setSurname(e.target.value)
+    }
+    let onChangeCF = (e) => {
+      // controllare se corretto il CF
+      setCF(e.target.value)
       return
     }
+    let onChangeBirthdate = (e) => {
+        setBirthday(e.target.value)
+    }
+
     let renderByType = () => {
       return(<>
-          <TextField id="filled-basic" label="Name"/>
-          <TextField id="filled-basic" label="Surname"/>
-          {userType == "patient" && 
-            <TextField id="filled-basic" label="Fiscal Code"/>}
-          <TextField id="date" label="Birthday" defaultValue="1980-01-01"   InputLabelProps={{shrink: true,}}/>
+          <TextField id="filled-basic" label="Name" onChange={(e)=>{onChangeName(e)}}/>
+          <TextField id="filled-basic" label="Surname" onChange={(e)=>{onChangeSurname(e)}}/>
+          {(userType === "Patient") && <TextField onChange={(e)=> onChangeCF(e)} id="filled-basic" label="Fiscal Code"/>}
+          <TextField id="date" onChange={(e) => onChangeBirthdate(e)} label="Birthday" defaultValue="1980-01-01"   InputLabelProps={{shrink: true,}}/>
+          <Button id={4} onClick={()=> submit()} variant={"contained"} color={"secondary"}>Submit Form</Button>
       </>)
     }
     return(<form>
@@ -43,6 +51,7 @@ function FirstAccess({user}){
         <Button id={2} onClick={(e) => buttonClick("Nurse")} variant={"contained"} color={"primary"}>Nurse</Button>
         <Button id={3} onClick={(e) => buttonClick("Patient")} variant={"contained"} color={"primary"}>Patient</Button>
         {renderByType()}
+        
       </form>)
 }
 
