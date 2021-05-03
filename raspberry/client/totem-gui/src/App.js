275444729c1267ee6925788c55c24cde4c701255
GiovanniColonni <giovacolo97@gmail.com>
2021-05-03 15:06:07 +0200

    riordinato repo e creazione client raspberry

--- raspberry/client/totem-gui/src/App.js
+++ raspberry/client/totem-gui/src/App.js
@@ -4,11 +4,54 @@ import Login from "./components/Login"
 
 
 function App() {
+  const [loginState,setLoginState] = useState(false)
+  const [user,setUser] = useState({})
+  const [username,setUserName] = useState("")
+  
+  let history = useHistory()
+
+  useEffect( () => {
+      async function checkUser(){
+          API.isAuthenticated()
+            .then((userJson) =>{ 
+                     
+                     if(userJson.id === null){
+                      setLoginState(false)
+                      history.push("/login")
+                    }else{
+                      setLoginState(true)
+                      setUserName(userJson.username)
+                      setUser(userJson)
+                      const t = userJson["userType"]
+                      if(t == "unknow"){
+                          history.push("/firstAccess")
+                      }
+                    }
+                     
+                    })
+            .catch((err)=> {
+            setLoginState(false) 
+            history.push("/login")
+            console.log(err)})
+            
+          }
+
+        checkUser()
+      
+    },[loginState,setUser,setUserName]
+  ) 
+
   return (
     <div className="App">
       <Switch>
           <Route exact path={"/login"}>
-              <Login />
+              <Login setLoginState={setLoginState} setUser={setUser} loginState={loginState}/>
+          </Route>
+          <Route exact path={"home"}>
+          <div>
+            <h1>Home of {username}</h1>
+            {loginState && <h1>Protected</h1>}
+          </div>
           </Route>
       </Switch>
     </div>
--- totem/server_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# se si ha ufw > ufw allow 5000
-uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app
-
