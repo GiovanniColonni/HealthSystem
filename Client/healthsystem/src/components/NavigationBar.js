0fd13fa2bf4aae1de0196fc981d420dd121da1ba
Arthemises <coralie.allioux@gmail.com>
2021-05-13 15:32:39 +0200

    Moving Logo component into Navbar component

--- Client/healthsystem/src/components/NavigationBar.js
+++ Client/healthsystem/src/components/NavigationBar.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import Image from 'react-bootstrap/Image'
 import NotificationModal from './NotificationModal';
-import Logo from './Logo';
+import Image from 'react-bootstrap/Image'
+import CrossIcon from '../icons/greenCross.png';
+
+var logostyle = {
+    logo: {
+        paddingTop: "2px",
+        paddingBottom: "2px",
+        paddingRight: "2px",
+        paddingLeft: "2px",
+    }, image: {
+        height: "50px",
+        width: "50px",
+    }, name: {
+        fontFamily: "Bree Serif",
+        color: "#8BC24A",
+        fontStyle: "italic",
+        fontWeight: "bold",
+        fontSize: "14px",
+        marginBottom: "0"
+    }
+}
+
+function Logo() {
+    return (
+        <>
+            <div style={logostyle.logo}>
+                <Image src={CrossIcon} style={logostyle.image}/>
+                <p style={logostyle.name}>My Health way</p>
+            </div>
+        </>
+    );
+}
 
 var navstyle = {
     nav: {
