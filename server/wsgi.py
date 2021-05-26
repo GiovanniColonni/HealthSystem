74af146a7292afa927d41ca2b1272d99982110ba
GiovanniColonni <giovacolo97@gmail.com>
2021-05-26 14:54:30 +0200

    aggiunto certificato per connessione https

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,5 +1,5 @@
 from app import app
-from config import FLASK_HOST
+from config import FLASK_HOST,CERT_PATH, KEY_PATH
 
 if __name__ == "__main__":
-    app.run(host=FLASK_HOST)
+    app.run(host=FLASK_HOST,ssl_context=(CERT_PATH,KEY_PATH))
