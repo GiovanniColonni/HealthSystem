7709130b76d5b1d299aab810a75ffdb5c6c302f8
GiovanniColonni <giovacolo97@gmail.com>
2021-06-03 18:24:22 +0200

    aggiunta app

---
+++ Client/HealthSystemMobile/components/Home.js
@@ -0,0 +1,23 @@
+import React,{useContext, useEffect} from "react"
+import { StyleSheet, Text, View,Button} from 'react-native';
+import UserContext from "../contexts/UserContext"
+
+function Home({navigation}){
+    // qui mettere registrazione notifica
+    let userState = useContext(UserContext)
+    useEffect(()=>{
+        
+    if(userState.user.googleId === ""){
+        navigation.navigate("Login")
+    }
+    }
+    )
+    return(
+        <View>
+            <Text>Home</Text>
+            <Button title="Go to MueasureHistory" onClick={navigation.navigate("MeasureHisotry")}/>
+        </View>
+        
+    )
+}
+export default Home;
