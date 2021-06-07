d0c7ee723f22dbae4abe454558c13fbe0f56fc2b
Arthemises <coralie.allioux@gmail.com>
2021-06-07 13:32:10 +0200

    Appointment creation OK

--- Client/healthsystem/src/components/AppointmentCard.js
+++ Client/healthsystem/src/components/AppointmentCard.js
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Item } from '@mui-treasury/components/flex';
 import IconButton from '@material-ui/core/IconButton';
-import API_patient from '../api/API_patient';
-import API_doctor from '../api/API_doctor';
-import API from '../api/API';
-import Doctor from '../classes/Doctor';
-import moment from 'moment';
-import { Typography } from '@material-ui/core';
 import {FaCheck} from 'react-icons/fa';
 
 var cardstyle = { 
@@ -39,7 +33,7 @@ var cardstyle = {
     }
 }
   
-export function AppointmentCard(props) {
+export default function AppointmentCard(props) {
   return (
     <Row gap={2} p={2.5} style={cardstyle.border}>
       <Item grow minWidth={0}>
@@ -55,82 +49,4 @@ export function AppointmentCard(props) {
       </Row>
     </Row>
   );
-}
-
-export function AppointmentCardList({user}) {
-    const initAppointmentList = () => {
-        const initList = [];
-        for (var days = 1; days < 8; days++) {
-            const currentDay = moment().startOf('day').add(days, 'days');
-            console.log("Current day: " + currentDay.format('dddd MMMM Do YYYY, h:mm a'))
-            if (currentDay.format('dddd') != "Saturday" && currentDay.format('dddd') != "Sunday") {
-                for (var hours = 8; hours < 18; hours++) {
-                    if (hours != 13){
-                        console.log("Current hour: " + hours)
-                        initList.push(moment().startOf('day').add(days, 'days').add(hours, 'hours').format('dddd MMMM Do YYYY, h:mm a'));
-                    }
-                }
-            }
-        }
-        return initList;
-    }
-
-    const [freeAppointmentList, setFreeAppointmentList] = useState(initAppointmentList());
-    const [docAppointmentList, setdocAppointmentList] = useState([]);
-    const [doctor, setDoctor] = useState(new Doctor());
-
-    useEffect(() => {
-        // REMEMBER to change the doctorId=6; retrieve it from cookies
-        if (user.googleId !== undefined){
-            API_patient.getPatient(user.googleId)
-            .then((patient) =>{
-            API_doctor.getDoctor(patient.doctorId)
-                .then((doct) =>{
-                setDoctor(doct)
-                })
-                .catch((err) =>{
-                console.log(err)
-                setDoctor()
-                })
-            })
-        }
-        if(doctor !== undefined){
-            API.getEvents(doctor.googleId, "Doctor")
-            .then((appointment) =>{
-            appointment.sort(function (left, right) {
-                    return moment.utc(right.date).diff(moment.utc(left.date))
-                });
-                setdocAppointmentList(appointment)
-                })
-                .catch((err) =>{
-                    setdocAppointmentList([])
-                    console.log(err)
-                })
-        }
-        /*if (docAppointmentList !== undefined){
-            console.log("Doc List" + docAppointmentList);
-            for (const busyAppointment of docAppointmentList) {
-                removeAppointment(busyAppointment.dateStart);
-            }
-            console.log("Free List " + freeAppointmentList); 
-        }*/
-    }, [user.googleId]);
-
-
-    const removeAppointment = (date) => {
-        const updatedList = freeAppointmentList.filter((appointmentDate) => appointmentDate !== date);
-        setFreeAppointmentList(updatedList);
-    }
-
-    return (
-        <>
-        {doctor !== undefined && freeAppointmentList.length > 0 &&
-            freeAppointmentList.map(appointment => (
-                <AppointmentCard title={appointment} caption={doctor.name +" "+doctor.surname}/>
-            ))
-        }
-        {freeAppointmentList.length == 0 &&
-            <Typography align="center" variant="h6">No Appointment Available</Typography>}
-        </>
-    );
 }
--- Client/healthsystem/src/components/NewAppointment.js
+++ Client/healthsystem/src/components/NewAppointment.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import BigCalendar from './BigCalendar';
 import { Row, Column, Item } from '@mui-treasury/components/flex';
 import TextField from '@material-ui/core/TextField';
 import { Typography } from '@material-ui/core';
 import { UserCardNoLink } from './UserCard';
 import API_doctor from '../api/API_doctor';
 import API_patient from '../api/API_patient';
+import API from '../api/API';
 import Button from '@material-ui/core/Button';
 import ModalFeedback from './ModalFeedback';
-import { AppointmentCardList } from './AppointmentCard';
+import moment from 'moment';
+import AppointmentCard from './AppointmentCard';
+
+export function AppointmentCardList({user}) {
+    
+
+    useEffect(() => {
+        // REMEMBER to change the doctorId=6; retrieve it from cookies
+        
+    }, [user.googleId]);
+
+
+    
+
+    
+}
 
 var newappstyle = {
     container: {
@@ -37,10 +52,28 @@ var newappstyle = {
 }
 
 export default function NewAppointment({user}){
-    
+    const initAppointmentList = () => {
+        const initList = [];
+        for (var days = 1; days < 8; days++) {
+            const currentDay = moment().startOf('day').add(days, 'days');
+            if (currentDay.format('dddd') != "Saturday" && currentDay.format('dddd') != "Sunday") {
+                for (var hours = 8; hours < 18; hours++) {
+                    if (hours != 13){
+                        initList.push(moment().startOf('day').add(days, 'days').add(hours, 'hours').format('dddd MMMM Do YYYY, h:mm a'));
+                    }
+                }
+            }
+        }
+        return initList;
+    }
+
+    const [freeAppointmentList, setFreeAppointmentList] = useState(initAppointmentList());
+    const [docAppointmentList, setdocAppointmentList] = useState([]);
     const [patient, setPatient] = useState({})
     const [doctor, setDoctor] = useState({})
     const [modalShow, setModalShow] = useState(false);
+    const [dateStart, setDateStart] = useState();
+    const [dateEnd, setDateEnd] = useState();
 
     useEffect(() => {
         API_patient.getPatient(user.googleId)
@@ -58,8 +91,83 @@ export default function NewAppointment({user}){
         .catch((err)=>{
             console.log(err)
         });
+        
+        if (user.googleId !== undefined){
+            API_patient.getPatient(user.googleId)
+            .then((patient) =>{
+            API_doctor.getDoctor(patient.doctorId)
+                .then((doct) =>{
+                setDoctor(doct)
+                })
+                .catch((err) =>{
+                console.log(err)
+                setDoctor()
+                })
+            })
+        }
+        if(doctor !== undefined){
+            API.getEvents(doctor.googleId, "Doctor")
+            .then((appointment) =>{
+            appointment.sort(function (left, right) {
+                    return moment.utc(right.date).diff(moment.utc(left.date))
+                });
+                setdocAppointmentList(appointment)
+                console.log(appointment)
+                })
+                .catch((err) =>{
+                    setdocAppointmentList([])
+                    console.log(err)
+                })
+        }
+        /*if (docAppointmentList !== undefined){
+            console.log("Doc List" + docAppointmentList);
+            for (const busyAppointment of docAppointmentList) {
+                removeAppointment(busyAppointment.dateStart);
+            }
+            console.log("Free List " + freeAppointmentList); 
+        }*/
     }, [user.googleId, patient.doctorId]);
 
+    const selectedAppointment = (date) => {
+        const formatedStartDate = moment(date, "MM/DD/YYYY hh:mm");
+        setDateStart(formatedStartDate.format("MM/DD/YYYY hh:mm"));
+        const formatedEndDate = formatedStartDate.add(1, 'hours');
+        setDateEnd(formatedEndDate.format("MM/DD/YYYY hh:mm"));
+    }
+
+    const removeAppointment = (date) => {
+        const updatedList = freeAppointmentList.filter((appointmentDate) => appointmentDate !== date);
+        setFreeAppointmentList(updatedList);
+    }
+
+    const AppointmentList = () =>{ 
+        return (
+            <>
+            {doctor !== undefined && freeAppointmentList.length > 0 &&
+                freeAppointmentList.map(appointment => (
+                    <AppointmentCard title={appointment} caption={doctor.name +" "+doctor.surname} 
+                        onClick={() => selectedAppointment(appointment)}/>
+                ))
+            }
+            {freeAppointmentList.length == 0 &&
+                <Typography align="center" variant="h6">No Appointment Available</Typography>}
+            </>
+        );
+    }
+
+    const validateAppointment = () => {
+        API_patient.setAppointment(patient.googleId,patient.doctorId,dateStart,"meeting",
+                                    "my description",dateEnd,"Let's wait")
+            .then((resp) =>{
+                if(resp.status === 200){
+                    setModalShow(true)
+                }
+                return true
+            }).catch((err) =>{
+                console.log(err);
+                return false;
+            })
+    }
 
     return(
         <>
@@ -91,7 +199,7 @@ export default function NewAppointment({user}){
                             variant="contained"
                             color="secondary"
                             style={newappstyle.okbutton}
-                            onClick={() => setModalShow(true)}
+                            onClick={() => validateAppointment()}
                         >
                             Validate
                         </Button>
@@ -103,7 +211,7 @@ export default function NewAppointment({user}){
                     </Row>
                 </Column>
                 <Column style={newappstyle.calendar}  gap={'inherit'} p={'inherit'}>
-                    <AppointmentCardList user={user} />
+                    <AppointmentList user={user} />
                 </Column>
             </Row>
             
