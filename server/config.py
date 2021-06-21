70a5e859ab6fd7f99d6a38459cee453e45e26923
luca97vale <51403422+luca97vale@users.noreply.github.com>
2021-06-21 10:56:49 +0200

    Update config.py

--- server/config.py
+++ server/config.py
@@ -8,7 +8,7 @@ GOOGLE_CLIENT_ID = "844949237967-h0pnqs3orkq4159ngua6s4jp0fdqatl4.apps.googleuse
 GOOGLE_ANDROID_ID = "844949237967-q118p10ptsdbt2rhf3s3gjru910jcdt4.apps.googleusercontent.com"
 GOOGLE_IOS_ID = "844949237967-c1vqt61lrrrc6qnh1b44eq2r4tq8fma7.apps.googleusercontent.com"
 # DB constans
-DB_HOST_ADDRESS = '79.42.64.83'
+DB_HOST_ADDRESS = '79.20.188.69'
 DB_USER = 'root'
 DB_PASSWORD = 'pwd'
 DB_NAME = 'remoteMonitoring'
