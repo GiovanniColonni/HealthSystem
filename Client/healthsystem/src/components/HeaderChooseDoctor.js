1cfdf5a44e7c1ba8938aeec7c4ba102564da1fed
Arthemises <coralie.allioux@gmail.com>
2021-06-22 08:36:55 +0200

    Fix layout header

--- Client/healthsystem/src/components/HeaderChooseDoctor.js
+++ Client/healthsystem/src/components/HeaderChooseDoctor.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import Logo from './Logo';
+import Typography from '@material-ui/core/Typography';
+import { Row, Column } from '@mui-treasury/components/flex';
 
+
+const headerstyle = {
+    item: {
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    green: {
+        color: '#61AB45'
+    }
+}
 
 const HeaderChooseDoctor = (props) =>{
     let {username} = props
     return (
-        <div>
-            <Logo />
-            <h1>Welcome {username}!</h1>
-            <h1>To complete your profile, choose a doctor</h1>
-            <h2>This will be your family doctor, who will follow you</h2>
-        </div>
+        <>
+        <Column p={3}>
+            <Row p={1}>
+                <Logo />
+            </Row>
+            <Row style={headerstyle.item}>
+                <Typography variant='h3'>Welcome {username}!</Typography>
+            </Row>
+            <Row style={headerstyle.item}>
+                <Typography variant='h3'>To complete your profile, choose a doctor</Typography>
+            </Row>
+            <Row style={headerstyle.item}>
+                <Typography style={headerstyle.green} gutterBottom>This will be your family doctor, who will follow you</Typography>
+            </Row>
+        </Column>
+        </>
     );
 }
 export default HeaderChooseDoctor;
