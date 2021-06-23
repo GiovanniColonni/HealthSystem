74a0c09dce54a4983f34ed9320c93c0855fd390f
Luca Valentini <luca97vale@gmail.com>
2021-06-23 12:43:34 +0200

    fix .pdf extension

--- server/blueprint/patient.py
+++ server/blueprint/patient.py
@@ -53,7 +53,7 @@ def get_prescription_file(pathFileSystem):
         my_file = Path("prescriptions/"+patientId+"/" + image)
         if my_file.is_file():
             # file exists
-            return send_file("prescriptions/"+patientId+"/" + image, mimetype='application/pdf')
+            return send_file("prescriptions/"+patientId+"/" + image)
     return make_response(jsonify("prescription not found"), 404)
 
 
