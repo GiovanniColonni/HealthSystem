5017bfc84407330836081b8fddc6e4623eb54d68
GiovanniColonni <giovacolo97@gmail.com>
2021-07-02 14:39:27 +0200

    risoluzione problema update raspberry

--- raspberry/client/totem-gui/src/components/Measure.js
+++ raspberry/client/totem-gui/src/components/Measure.js
@@ -43,7 +43,7 @@ function Measure ({setMeasure, measure, user, handleLogout}) {
     let first = true
     let [name,setName] = useState("")
 
-    let [message,setMessage] = useState("first")
+    let [message,setMessage] = useState("Inizia Misura")
 
     let x = [1,2,3,4,7,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
     
