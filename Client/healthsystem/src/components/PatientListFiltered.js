9dc7a152ae5ddeea1e8a311023596d6239731ae7
Arthemises <coralie.allioux@gmail.com>
2021-07-06 11:53:29 +0200

    Adding filter by name in patient list screen

--- Client/healthsystem/src/components/PatientListFiltered.js
+++ Client/healthsystem/src/components/PatientListFiltered.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { UserCardList } from './UserCard';
 import {Input } from '@material-ui/core';
 
-export default function PatientList({user,filter, setPatient}) {
+export default function PatientListFiltered({user}) {
     const [patientNameFilter, setPatientNameFilter] = useState("")
     return (
         <div>
             <Input placeholder="Patient Name" name="patientName" onChange={(e) => setPatientNameFilter(e.target.value) } />
-            <UserCardList user={user} filter={patientNameFilter} setPatient={(patientId) => setPatient(patientId)}/>
+            <UserCardList user={user} filter={patientNameFilter} />
         </div>
     )
 }
