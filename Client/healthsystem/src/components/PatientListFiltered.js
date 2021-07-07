61c5c7868806807f6176a9df9d9890f368632afa
Arthemises <coralie.allioux@gmail.com>
2021-07-07 08:03:25 +0200

    Layout patient list with filter

--- Client/healthsystem/src/components/PatientListFiltered.js
+++ Client/healthsystem/src/components/PatientListFiltered.js
@@ -1,13 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { UserCardList } from './UserCard';
-import {Input } from '@material-ui/core';
+import { Input, IconButton } from '@material-ui/core';
+import { GoSearch } from 'react-icons/go';
+
+const style = {
+    cardList: {
+        width: '50%',
+        margin: 'auto'
+    }
+}
 
 export default function PatientListFiltered({user}) {
     const [patientNameFilter, setPatientNameFilter] = useState("")
     return (
         <div>
+            <IconButton disabled>
+                <GoSearch/>
+            </IconButton>
             <Input placeholder="Patient Name" name="patientName" onChange={(e) => setPatientNameFilter(e.target.value) } />
-            <UserCardList user={user} filter={patientNameFilter} />
+            
+            <div style={style.cardList}>
+                <UserCardList user={user} filter={patientNameFilter} />
+            </div>
         </div>
     )
 }
