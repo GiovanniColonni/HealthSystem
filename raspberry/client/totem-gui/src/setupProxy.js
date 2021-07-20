21770e050b269d7ea7a19972661ccbfd958588cf
GiovanniColonni <giovacolo97@gmail.com>
2021-07-20 11:34:29 +0200

    ips

--- raspberry/client/totem-gui/src/setupProxy.js
+++ raspberry/client/totem-gui/src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require("http-proxy-middleware")
 
 module.exports = function(app){
-    app.use(proxy("/api",{target: "https://172.20.10.6:5000",secure:false}))
-    app.use(proxy("/totem",{target:"https://172.20.10.14:5001",secure:false}))
+    app.use(proxy("/api",{target: "https://192.168.43.189:5000",secure:false}))
+    app.use(proxy("/totem",{target:"https://192.168.43.221:5001",secure:false}))
 }
